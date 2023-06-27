f38790cfb0884cf45d07df1c00ed7905a9af4b50
kirankumarkarmala1996 <108362315+kirankumarkarmala1996@users.noreply.github.com>
2023-06-27 09:37:25 +0530

    demoservlet

---
+++ src/main/java/com/controler/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.controler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pwri = resp.getWriter();
+		pwri.println("<h1>HelloWorldServlet welcome to servlet project</h1>");
+		pwri.close();
+
+	}
+
+}
---
+++ src/main/java/com/controler/WelcomeServlet.java
@@ -0,0 +1,21 @@
+package com.controler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pwri = resp.getWriter();
+		pwri.println("<h1>welcome to servlet projectDemo</h1>");
+		pwri.close();
+
+	}
+
+}
